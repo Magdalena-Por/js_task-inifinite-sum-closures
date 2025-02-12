e8bc0b9b65906dd27d64d784a621e73e5e4c6610
Magdalena-Por <anna.meex@gmail.com>
2025-02-12 12:14:22 +0100

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,27 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0; // Variable to store the sum of numbers
+
+  // This is the adder function that will handle chaining
+  function adder(number) {
+    if (number === undefined) {
+      // If no argument is passed, return the sum and reset it
+      const result = sum;
+
+      sum = 0; // Reset sum after returning it
+
+      return result;
+    } else {
+      // If a number is passed,
+      // add it to the sum and return the adder for chaining
+      sum += number;
+
+      return adder; // Return the function itself for chaining
+    }
+  }
+
+  return adder; // Return the adder function
 }
 
 module.exports = makeInfinityAdder;
